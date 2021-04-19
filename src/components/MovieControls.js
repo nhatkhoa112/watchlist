fe0c681790cd5fc75e26dd4f6615dfeccc8fee1a
nhat khoa <nhatkhoa.tin@gmail.com>
2021-04-19 20:35:31 +0700

    initial commit

---
+++ src/components/MovieControls.js
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+const MovieControls = ({movie, type}) => {
+    const { removeMovieFromWatchlist, addMovieToWatched } = useContext(GlobalContext);
+
+
+    return (
+        <div className="inner-card-controls"> 
+            {type === 'watchlist' && (
+                <>
+                    <button 
+                        onClick={() => addMovieToWatched(movie.id)}
+                        className="ctrl-btn">
+                        <i className="fa-fw far fa-eye"></i>
+                    </button>
+
+                    <button 
+                        onClick={() => removeMovieFromWatchlist(movie.id)}
+                        className="ctrl-btn">
+                        <i className="fa-fw fa fa-times"></i>
+                    </button>
+                    
+                </>
+            )}
+        </div>
+    )
+}
+
+export default MovieControls
---
+++ src/components/WatchList.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import {GlobalContext} from '../context/GlobalState';
+import MovieCard from './MovieCard';
+
+const WatchList = () => {
+    const { watchlist } = useContext(GlobalContext);
+    console.log(watchlist)
+
+    return (
+        <div className="movie-page">
+            <div className="container">
+                <div className="header">
+                    <h1 className="heading">My WatchList</h1>
+                </div>
+                {watchlist.length > 0 ? (
+                    <div className="movie-grid">
+                        {watchlist.map(movie => {
+                            return <MovieCard key={movie.id} movie={movie} type="watchlist" />
+                        })}
+                    </div>
+                ) : (
+                    <h2 className="no-movies"> No movies in your list, add some ... </h2>
+                )}
+
+            </div>
+        </div>
+    )
+}
+
+export default WatchList
